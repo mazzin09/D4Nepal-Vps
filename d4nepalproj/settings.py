bd6ff0657cdb34e8bda9222570f20ab9a64d6788
majin byanjankar <majin.byanjankar@gmail.com>
2024-01-04 10:31:41 +0545

    add mysql file and code in settings.py

--- d4nepalproj/settings.py
+++ d4nepalproj/settings.py
@@ -120,10 +120,18 @@ WSGI_APPLICATION = 'd4nepalproj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "OPTIONS": {
+            "read_default_file": "/path/to/my.cnf",
+        },
     }
 }
 
